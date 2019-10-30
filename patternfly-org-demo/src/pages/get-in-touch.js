1481d2f92ba9371d555055bfbb6033a2484b1133
evwilkin <evan.wilkinson@gmail.com>
2019-10-30 10:27:10 -0400

    get in touch: fixed page height

--- patternfly-org-demo/src/pages/get-in-touch.js
+++ patternfly-org-demo/src/pages/get-in-touch.js
@@ -13,6 +13,7 @@ const GetInTouch = ({ location }) => (
     <PageSection
       variant={PageSectionVariants.light}
       className="ws-section"
+      style={{height: "calc(100vh - 92px)"}}
     >
       <h1 className="pf-u-mb-lg ws-title">Get in touch</h1>
 
@@ -20,7 +21,7 @@ const GetInTouch = ({ location }) => (
 
       <p className="ws-mdx-p">We are here to help.</p>
 
-      <Grid sm={12} md={6} gutter="sm" class="pf-u-my-lg pf-l-grid pf-m-all-12-col-on-sm pf-m-all-6-col-on-md pf-m-gutter" style={{ maxWidth: '450px' }}>
+      <Grid sm={12} md={6} gutter="sm" className="pf-u-my-lg pf-l-grid pf-m-all-12-col-on-sm pf-m-all-6-col-on-md pf-m-gutter" style={{ maxWidth: '450px' }}>
         <GridItem>
           <Split>
             <SplitItem style={{ marginRight: '12px' }}><h3><ChatIcon /></h3></SplitItem>
